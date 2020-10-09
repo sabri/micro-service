d35e48c6f5d270767094bd4f83e578696ef89018
sabri baazaoui <baazaouisabri@gmail.com>
2020-10-10 00:27:44 +0100

    Init 3 microservices

---
+++ src/main/java/com/example/moviecatlogapi/MovieCatlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviecatlogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCatlogApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieCatlogApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/moviecatlogapi/MovieCatlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviecatlogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatlogApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
