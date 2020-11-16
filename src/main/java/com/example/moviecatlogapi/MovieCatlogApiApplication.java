cd95f870117b593447c6481c7cce4675550b9094
sabri baazaoui <baazaouisabri@gmail.com>
2020-11-16 21:11:29 +0100

    Init 3 microservice

--- src/main/java/com/example/moviecatlogapi/MovieCatlogApiApplication.java
+++ src/main/java/com/example/moviecatlogapi/MovieCatlogApiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MovieCatlogApiApplication {
+    
 
     public static void main(String[] args) {
         SpringApplication.run(MovieCatlogApiApplication.class, args);
